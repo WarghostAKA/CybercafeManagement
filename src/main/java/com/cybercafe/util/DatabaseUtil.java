78b8626c089c5212cd33f30377c9b5bad4a70cfe
warghostaka <2915701431@qq.com>
2024-12-20 20:19:56 +0800

    修复了数据库uml配置错误问题

--- src/main/java/com/cybercafe/util/DatabaseUtil.java
+++ src/main/java/com/cybercafe/util/DatabaseUtil.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseUtil {
-    private static final String URL = "jdbc.url=jdbc:mysql://localhost:3306/cybercafe_db?useUnicode=true&characterEncoding=gb2312";
+    private static final String URL = "jdbc:mysql://localhost:3306/cybercafe_db?useUnicode=true&characterEncoding=gb2312";
     private static final String USER = "root";
     private static final String PASSWORD = "313268";
 
